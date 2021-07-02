e45af6067b9b99945000d33074f35fcbf2b9032d
gillmourtunhira <gillmourtunhira@gmail.com>
2021-07-02 15:34:01 +0200

    mobile class for menu

--- js/lands.js
+++ js/lands.js
@@ -1,1 +1,9 @@
-console.log( 'Hello DevMaster' );
+console.log('Hello DevMaster')
+
+const buttonMenu = document.querySelector('.mobile__menu--nav')
+const mobileMenu = document.querySelector('.main__menu--container nav')
+
+buttonMenu.addEventListener('click', (e) => {
+  e.preventDefault()
+  mobileMenu.classList.toggle('open__mobile')
+})
