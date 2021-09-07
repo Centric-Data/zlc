d0ad67d174a2d5662d06729de257d49a29fa6a81
gillmourtunhira <gillmourtunhira@gmail.com>
2021-09-07 16:16:35 +0200

    moved doclense css

--- js/lands.js
+++ js/lands.js
@@ -10,26 +10,3 @@ buttonMenu.addEventListener(
     mobileMenu.classList.toggle('open__mobile')
   }
 );
-
-// const counters = document.querySelectorAll('.statistics__counter');
-// const speed = 200;
-
-// counters.forEach( counter => {
-//   const updateCount = () => {
-//     const target = +counter.getAttribute('data-target');
-
-//     const count = +counter.innerText;
-
-//     const inc = target / speed;
-
-//     if( count < target ) {
-//       counter.innerText = count + inc;
-//       setTimeout( updateCount, 1 );
-//     } else {
-//       count.innerText = target;
-//     }
-//   }
-
-//   updateCount();
-// } );
-
